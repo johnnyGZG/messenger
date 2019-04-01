16f4375bd4acf33ac63b331e42db0f1c280cc9d1
Johnny <power-gabriel11@hotmail.es>
2019-03-31 22:10:45 -0500

    Creando controladores 'php artisan make:controller NombreController'

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,6 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/api/conversaciones', 'ConversationController@index')->name('conversations');
+Route::get('/api/mensajes', 'MessageController@index')->name('messages');
